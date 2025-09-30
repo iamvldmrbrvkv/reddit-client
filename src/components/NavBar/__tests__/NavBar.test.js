fd68bdbdd5391c6e7747965b63483473a50f8e6f
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2025-09-30 14:05:53 +0300

    Refactor NavBar tests to remove redundant render calls by moving them into individual test cases

--- src/components/NavBar/__tests__/NavBar.test.js
+++ src/components/NavBar/__tests__/NavBar.test.js
@@ -2,24 +2,25 @@ import { render, screen } from '@testing-library/react';
 import NavBar from '../NavBar';
 import { BrowserRouter } from 'react-router-dom';
 
-beforeEach(() => {
-  render(<NavBar />, { wrapper: BrowserRouter })
-})
 
 describe('renders NavBar elements', () => {
   test('logo', () => {
+    render(<NavBar />, { wrapper: BrowserRouter })
     expect(screen.getByAltText('logo')).toBeInTheDocument()
   })
 
   test('title', () => {
+    render(<NavBar />, { wrapper: BrowserRouter })
     expect(screen.getByText(/reddit client/i)).toBeInTheDocument()
   })
 
   test('links', () => {
+    render(<NavBar />, { wrapper: BrowserRouter })
     expect(screen.getAllByRole('link')).toHaveLength(4)
   })
 
   test('input', () => {
+    render(<NavBar />, { wrapper: BrowserRouter })
     expect(screen.getByPlaceholderText('Search Reddit')).toBeInTheDocument()
   })
 })
